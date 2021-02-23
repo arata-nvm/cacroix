8e702b06585b601d5e9f49f06b6ba1f42cb94361
arata <arata04962@gmail.com>
2021-02-23 18:57:13 +0900

    :sparkles: update demos

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -1,15 +1,22 @@
 use cacroix::{
     common::shape,
     dynamic::{
-        particle::{Material, Particle},
+        joint::{distance::DistanceJoint, mortor::MortorJoint, Joint},
+        particle::{self, Material, Particle},
         world::{DefaultWorld, World},
     },
 };
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
+use piston::{
+    event_loop::{EventSettings, Events},
+    keyboard, PressEvent,
+};
+use piston::{
+    input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent},
+    Button,
+};
 
 pub struct App {
     gl: GlGraphics,
@@ -82,7 +89,7 @@ fn main() {
 
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        world: init_world2(),
+        world: init_world(),
     };
 
     let mut events = Events::new(EventSettings {
@@ -97,7 +104,15 @@ fn main() {
         if let Some(args) = e.update_args() {
             app.update(&args);
         }
+
+        if let Some(Button::Keyboard(keyboard::Key::Space)) = e.press_args() {
+            app.world = init_world();
+        }
     }
+}
+
+fn init_world() -> DefaultWorld {
+    return init_world4();
 }
 
 fn init_world1() -> DefaultWorld {
@@ -107,12 +122,12 @@ fn init_world1() -> DefaultWorld {
     let p1 = [100.0, 100.0];
     let p2 = [200.0, 200.0];
     let size = 2.0;
-    let mass = 200.0;
+    let mass = 20.0;
     let material = Material {
         linear_damping: 0.999,
         restitution: 0.25,
     };
-    let strength = 0.1;
+    let strength = 1.5;
     shape::new_rect(&mut world, p1, p2, size, mass, material, strength);
 
     let center = [300.0, 300.0];
@@ -143,5 +158,55 @@ fn init_world2() -> DefaultWorld {
         ));
     }
 
+    return world;
+}
+
+fn init_world3() -> DefaultWorld {
+    let gravity = [0.0, 9.8];
+    let mut world: DefaultWorld = World::new(800, 600, gravity);
+
+    let m = Material {
+        linear_damping: 0.999,
+        restitution: 0.1,
+    };
+
+    let p1 = world.add_particle(Particle::new([400.0, 300.0], 10.0, 10.0, m));
+    let p2 = world.add_particle(Particle::new([400.0, 400.0], 10.0, 10.0, m));
+    world.add_joint(Box::new(MortorJoint::new(&p1, &p2, 100.0, 1.0)));
+
+    for i in 0..10 {
+        world.add_particle(Particle::new([80.0 * i as f64, 500.0], 10.0, 10.0, m));
+    }
+
+    p1.borrow_mut().typ = particle::Type::Static;
+
+    return world;
+}
+
+fn init_world4() -> DefaultWorld {
+    let gravity = [0.0, 9.8];
+    let mut world: DefaultWorld = World::new(800, 600, gravity);
+
+    let m = Material {
+        linear_damping: 0.999,
+        restitution: 0.1,
+    };
+
+    let mut p = Vec::new();
+    for i in 0..10 {
+        p.push(world.add_particle(Particle::new(
+            [80.0 * i as f64 + 40.0, 100.0],
+            10.0,
+            10.0,
+            m,
+        )));
+    }
+
+    p.first_mut().unwrap().borrow_mut().typ = particle::Type::Static;
+    p.last_mut().unwrap().borrow_mut().typ = particle::Type::Static;
+
+    for i in 0..9 {
+        world.add_joint(Box::new(DistanceJoint::new(&p[i], &p[i + 1], 80.0, 2.0)));
+    }
     return world;
 }
